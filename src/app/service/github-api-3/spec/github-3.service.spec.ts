05c4614b4cacf71b11a1f7c66939e4d1c090c515
Clayton Passos <netstart@gmail.com>
2018-04-19 22:57:42 -0300

    adicionando testes para verificar se a url foi gerada corretamente

--- src/app/service/github-api-3/spec/github-3.service.spec.ts
+++ src/app/service/github-api-3/spec/github-3.service.spec.ts
@@ -1,16 +1,30 @@
-import {TestBed, inject} from '@angular/core/testing';
-
 import {Github3Service} from '../github-3.service';
+import {HttpClient} from '@angular/common/http';
 
 describe('Github3Service', () => {
 
   let github3Service: Github3Service;
+  let httpClientModuleSpy;
   beforeEach(() => {
-    github3Service = new Github3Service(null, 'repofake');
+    httpClientModuleSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    github3Service = new Github3Service(httpClientModuleSpy, 'frontendbr/vagas');
   });
 
   it('should be created', () => {
     expect(github3Service).toBeTruthy();
+  });
+
+  it('Should be call 1', () => {
+    github3Service.issues('state=open');
+    expect(httpClientModuleSpy.get.calls.count())
+      .toBe(1, 'Deveria ter chamado o get uma vez');
+    expect(httpClientModuleSpy.get).toHaveBeenCalledTimes(1);
+    expect(httpClientModuleSpy.get).toHaveBeenCalled();
+  });
+
+  it('Should be generate full url and called with it', () => {
+    github3Service.issues('state=open');
+    expect(httpClientModuleSpy.get).toHaveBeenCalledWith('https://api.github.com/repos/frontendbr/vagas/issues?state=open');
   });
 
 });
