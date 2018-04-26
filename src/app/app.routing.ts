55c7434bd63aacdec08893d2541718e79301116b
Clayton K. N. Passos <netstart@gmail.com>
2018-04-26 13:42:36 -0300

    refactoring first router - Moving to page.routing.module

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,9 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SearchComponent} from './page/search/search.component';
 
-const appRoutes: Routes = [
-  {path: '', component: SearchComponent}
-];
+const appRoutes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
--- src/app/page/page.module.ts
+++ src/app/page/page.module.ts
@@ -1,9 +1,10 @@
 import {NgModule} from '@angular/core';
 import {SearchComponent} from './search/search.component';
 import {ShareModule} from '../share/share.module';
+import {PageRoutingModule} from './page.routing.module';
 
 @NgModule({
-  imports: [ShareModule],
+  imports: [ShareModule, PageRoutingModule],
   exports: [SearchComponent],
   declarations: [SearchComponent]
 })
