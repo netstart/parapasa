8397786d2de58be1caa3b011ea38a0818c9e8403
Clayton K. N. Passos <netstart@gmail.com>
2018-04-26 01:53:28 -0300

    clean unused component

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,14 +1,12 @@
 import {NgModule} from '@angular/core';
 import {AngularShareModule} from './angular-share/angular-share.module';
-import {HeaderComponent} from './component/layout/header/header.component';
-import {FooterComponent} from './component/layout/footer/footer.component';
 
 @NgModule({
   imports: [
     AngularShareModule
   ],
-  exports: [AngularShareModule, HeaderComponent, FooterComponent],
-  declarations: [HeaderComponent, FooterComponent]
+  exports: [AngularShareModule],
+  declarations: []
 })
 export class ShareModule {
 }
