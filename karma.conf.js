deeda89896df9460f5d35e94d3eb10def1c6094b
Restyled.io <commits@restyled.io>
2019-10-06 22:03:10 +0000

    Restyled by prettier

--- karma.conf.js
+++ karma.conf.js
@@ -1,31 +1,31 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/parapasa'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require("path").join(__dirname, "./coverage/parapasa"),
+      reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ["Chrome"],
     singleRun: false,
     restartOnFileChange: true,
 
@@ -37,29 +37,29 @@ module.exports = function (config) {
 
       //  ng test --browsers HeadlessChromium
       HeadlessChromium: {
-        base: 'Chromium',
+        base: "Chromium",
         flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222',
+          "--no-sandbox",
+          "--headless",
+          "--disable-gpu",
+          "--remote-debugging-port=9222"
         ]
       },
 
       //  ng test --browsers HeadlessChrome
       HeadlessChrome: {
-        base: 'Chrome',
+        base: "Chrome",
         flags: [
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222',
+          "--headless",
+          "--disable-gpu",
+          "--remote-debugging-port=9222"
           //'--window-size=1024,768'
         ]
       },
 
       ChromeTravisCi: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
+        base: "Chrome",
+        flags: ["--no-sandbox"]
       }
     }
   });
