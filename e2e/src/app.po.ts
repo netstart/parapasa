9ced19bbce40651e11cf61405aff91fb01be0f37
Clayton Passos <netstart@gmail.com>
2019-10-06 18:35:54 -0300

    updaate

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
+  public getTitleText() {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
 }
